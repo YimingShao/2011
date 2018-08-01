6afc1a81a13015a2cc7770dbb2c0c2bfc1d981de
Yiming Shao <shaoyiminglobo@gmailcom>
2018-08-01 17:23:05 -0400

    update

--- Sorting/api.py
+++ Sorting/api.py
@@ -2,7 +2,8 @@ class api:
 
     # Given an array A of n integers, sort them by repetively selecting the smallest among
     # the yet unselected integers.
-    def Selection_Sort(ary):
+    def Selection_Sort(my_ary):
+        ary = my_ary[:]
         n = len(ary)
         for i in range(n):
             min = i
@@ -13,9 +14,24 @@ class api:
                 ary[min], ary[i] = ary[i], ary[min]
         return ary
 
-
-    def Bubble_Sort(ary):
-        for i in range(len(ary)-1):
-            if ary[i] > ary[i+1]:
-                ary[i],ary[i+1] = ary[i+1], ary[i]
+    # Bubble sort is pretty slow, we don't want to use this algorithm in real problem
+    def Bubble_Sort(my_ary):
+        ary = my_ary[:]
+        for i in range(len(ary)):
+            for j in range(0, len(ary)-i-1):
+                if ary[j] > ary[j+1]:
+                    ary[j], ary[j+1] = ary[j+1], ary[j]
         return ary
+
+
+    def Insertion_Sort(my_ary):
+        ary = my_ary[:]
+        for i in range(1, len(ary)):
+            key = ary[i]
+            j=i-1
+            while j >=0 and key < ary[j]:
+                ary[j+1] = ary[j]
+                j = j-1
+            ary[j+1] = key
+        return ary
+
