ee529d472c3d9427ec8837f4673783daf67468f6
Yiming Shao <shaoyiminglobo@gmailcom>
2018-08-07 16:07:33 -0400

    update

--- Sorting/api.py
+++ Sorting/api.py
@@ -1,37 +1,49 @@
-class api:
+# Given an array A of n integers, sort them by repetively selecting the smallest among
+# the yet unselected integers.
+def Selection_Sort(my_ary):
+    ary = my_ary[:]
+    n = len(ary)
+    for i in range(n):
+        min = i
+        for j in range(i + 1, n):
+            if ary[min] > ary[j]:
+                min = j
+        if min != i:
+            ary[min], ary[i] = ary[i], ary[min]
+    return ary
 
-    # Given an array A of n integers, sort them by repetively selecting the smallest among
-    # the yet unselected integers.
-    def Selection_Sort(my_ary):
-        ary = my_ary[:]
-        n = len(ary)
-        for i in range(n):
-            min = i
-            for j in range(i + 1, n):
-                if ary[min] > ary[j]:
-                    min = j
-            if min != i:
-                ary[min], ary[i] = ary[i], ary[min]
-        return ary
-
-    # Bubble sort is pretty slow, we don't want to use this algorithm in real problem
-    def Bubble_Sort(my_ary):
-        ary = my_ary[:]
-        for i in range(len(ary)):
-            for j in range(0, len(ary)-i-1):
-                if ary[j] > ary[j+1]:
-                    ary[j], ary[j+1] = ary[j+1], ary[j]
-        return ary
+# Bubble sort is pretty slow, we don't want to use this algorithm in real problem
+def Bubble_Sort(my_ary):
+    ary = my_ary[:]
+    for i in range(len(ary)):
+        for j in range(0, len(ary)-i-1):
+            if ary[j] > ary[j+1]:
+                ary[j], ary[j+1] = ary[j+1], ary[j]
+    return ary
 
 
-    def Insertion_Sort(my_ary):
-        ary = my_ary[:]
-        for i in range(1, len(ary)):
-            key = ary[i]
-            j=i-1
-            while j >=0 and key < ary[j]:
-                ary[j+1] = ary[j]
-                j = j-1
-            ary[j+1] = key
-        return ary
+def merge(L, R):
+    res = []
+    while L and R:
+        if L[0] < R[0]:
+            res.append(L.pop(0))
+        else:
+            res.append(R.pop(0))
+    while L:
+        res.append(L.pop(0))
+    while R:
+        res.append(R.pop(0))
+    return res
 
+
+def merge_function(ary):
+    if len(ary) > 1:
+        mid = (int)(len(ary)/2)
+        L = mergesort(ary[:mid])
+        R = mergesort(ary[mid:])
+        return merge(L,R)
+    return ary
+
+def mergesort(my_ary):
+    ary = my_ary[:]
+    return merge_function(ary)
--- Sorting/prorgam.py
+++ Sorting/prorgam.py
@@ -1,16 +1,23 @@
-from api import api
+import api
 
 def main():
 
     num_ary = [5, 2, 2, 4, 8, 9, 3]
-    new_ary = api.Selection_Sort(num_ary)
+    # new_ary = api.Selection_Sort(num_ary)
+    # print(new_ary)
+    #
+    # new_ary = api.Bubble_Sort(num_ary)
+    # print(new_ary)
+    #
+    # new_ary = api.Insertion_Sort(num_ary)
+    # print(new_ary)
+
+    new_ary = api.mergesort(num_ary)
     print(new_ary)
 
-    new_ary = api.Bubble_Sort(num_ary)
-    print(new_ary)
 
-    new_ary = api.Insertion_Sort(num_ary)
-    print(new_ary)
+
+
 
 if __name__ == '__main__':
     main()
